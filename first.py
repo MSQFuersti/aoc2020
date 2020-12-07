ad47479733d1240f3d1b0a8a757bdf216ea78937
Maximilian Fürst <maximilian.fuerst@tngtech.com>
2020-12-07 22:33:39 +0100

    Use recursion for day 1 solution

--- first.py
+++ first.py
@@ -1,5 +1,7 @@
 import csv
 import math
+import time
+
 
 def getNumbers():
     with open('input.txt') as csv_file:
@@ -8,22 +10,31 @@ def getNumbers():
         return [int(item) for sublist in data for item in sublist]
 
 
-def getTwoNumbersSummingUpTo(targetSum, numbers):
-    for item in numbers:
-        if targetSum - item in numbers:
-            return [(targetSum - item), item]
-
-
-def getThreeNumbersSummingUpTo(targetSum, numbers):
-    for item in numbers:
+def getKNumbersSummingUpTo(targetSum, numbers, k):
+    for index, item in enumerate(numbers):
         remainingTarget = targetSum - item
-        searchedNumbers = getTwoNumbersSummingUpTo(remainingTarget, numbers)
-        if searchedNumbers:
-            searchedNumbers.append(item)
-            return searchedNumbers
+        if k == 2:
+            if remainingTarget in numbers:
+                return [(targetSum - item), item]
+
+        else:
+            searchedNumbers = getKNumbersSummingUpTo(remainingTarget, numbers, k - 1)
+            if searchedNumbers:
+                searchedNumbers.append(item)
+                return searchedNumbers
 
 
 numbers = getNumbers()
-productOfTwoNumbersSummingUpTo2020 = math.prod(getTwoNumbersSummingUpTo(2020, numbers))
-productOfThreeNumbersSummingUpTo2020 = math.prod(getThreeNumbersSummingUpTo(2020, numbers))
-print(productOfTwoNumbersSummingUpTo2020, productOfThreeNumbersSummingUpTo2020)
+numbers.extend([1, 2, 3, 4])
+print((numbers))
+start = time.time()
+productOfTwoNumbersSummingUpTo2020 = math.prod(getKNumbersSummingUpTo(2020, numbers, 2))
+timeTwo = time.time() - start
+start = time.time()
+productOfThreeNumbersSummingUpTo2020 = math.prod(getKNumbersSummingUpTo(2020, numbers, 3))
+timeThree = time.time() - start
+start = time.time()
+productOfFiveNumbersSummingUpTo2020 = math.prod(getKNumbersSummingUpTo(2020, numbers, 5))
+timeFive = time.time() - start
+print(productOfTwoNumbersSummingUpTo2020, productOfThreeNumbersSummingUpTo2020, productOfFiveNumbersSummingUpTo2020)
+print(timeTwo, timeThree, timeFive)
